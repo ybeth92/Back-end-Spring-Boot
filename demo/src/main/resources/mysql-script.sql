48eaf3e534c029115fea8d7a795bf91ed3c99ecd
Ybeth Chavez <ybth.92.15@gmail.com>
2021-04-16 18:13:24 -0500

    actualizacion del script

--- demo/src/main/resources/mysql-script.sql
+++ demo/src/main/resources/mysql-script.sql
@@ -2,9 +2,9 @@ create database comsatelbasictodo;
 use comsatelbasictodo;
 
 create table tarea(
-	id		int				auto_increment,
+    id		int				auto_increment,
     tarea	varchar(300)	not null,
-    estado	int				not null,
+    estado	int				not null default 0,
     PRIMARY KEY (id)
 );
 
